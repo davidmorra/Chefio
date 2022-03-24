68df12d5174aeeb54678153ef1ee39988a29366d
davidmorra <motogge@gmail.com>
2022-03-24 21:01:38 +0400

    Initial Commit

---
+++ Chefio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chefio
+//
+//  Created by Davit on 24.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
